8b1725cbf46a41204f198f345d8ddc3f0c815bc3
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-05-28 23:40:27 +0200

    Add swiper library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,22 @@
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from "swiper";
+
 // Navigation toggle
 window.addEventListener("load", function () {
+  const projectSlider = new Swiper(".projects", {
+    modules: [Navigation, Pagination],
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  const logoSlider = new Swiper(".references", {
+    modules: [Navigation, Pagination],
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
   let main_navigation = document.querySelector("#primary-menu");
   document
     .querySelector("#primary-menu-toggle")
