df2526102b71f2442e6d3041b6e63d4b47bb7ad2
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-05-28 23:48:52 +0200

    Refactor theme setup functions to align with new theme name

--- functions.php
+++ functions.php
@@ -3,12 +3,13 @@
 /**
  * Theme setup.
  */
-function tailpress_setup() {
-	add_theme_support( 'title-tag' );
+function symbos_setup()
+{
+	add_theme_support('title-tag');
 
 	register_nav_menus(
 		array(
-			'primary' => __( 'Primary Menu', 'tailpress' ),
+			'primary' => __('Primary Menu', 'symbos'),
 		)
 	);
 
@@ -23,17 +24,18 @@ function tailpress_setup() {
 		)
 	);
 
-    add_theme_support( 'custom-logo' );
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('custom-logo');
+	add_theme_support('post-thumbnails');
 
-	add_theme_support( 'align-wide' );
-	add_theme_support( 'wp-block-styles' );
+	add_theme_support('align-wide');
+	add_theme_support('wp-block-styles');
 
-	add_theme_support( 'editor-styles' );
-	add_editor_style( 'css/editor-style.css' );
+	add_theme_support('editor-styles');
+	add_editor_style('css/editor-style.css');
 }
 
-add_action( 'after_setup_theme', 'tailpress_setup' );
+add_action('after_setup_theme', 'symbos_setup');
+
 // Disable comment functionality
 
 add_action('admin_init', function () {
@@ -80,14 +82,17 @@ add_action('init', function () {
 /**
  * Enqueue theme assets.
  */
-function tailpress_enqueue_scripts() {
+function symbos_enqueue_scripts()
+{
 	$theme = wp_get_theme();
 
-	wp_enqueue_style( 'tailpress', tailpress_asset( 'css/app.css' ), array(), $theme->get( 'Version' ) );
-	wp_enqueue_script( 'tailpress', tailpress_asset( 'js/app.js' ), array(), $theme->get( 'Version' ) );
+	wp_enqueue_style('symbos', symbos_asset('css/app.css'), array(), $theme->get('Version'));
+
+	wp_enqueue_script('symbos', symbos_asset('js/app.js'), array(), $theme->get('Version'));
 }
 
-add_action( 'wp_enqueue_scripts', 'tailpress_enqueue_scripts' );
+add_action('wp_enqueue_scripts', 'symbos_enqueue_scripts');
+
 // Add fonts to <head>
 function symbos_preload_fonts()
 {
@@ -106,12 +111,13 @@ add_action('wp_head', 'symbos_preload_fonts');
  *
  * @return string
  */
-function tailpress_asset( $path ) {
-	if ( wp_get_environment_type() === 'production' ) {
+function symbos_asset($path)
+{
+	if (wp_get_environment_type() === 'production') {
 		return get_stylesheet_directory_uri() . '/' . $path;
 	}
 
-	return add_query_arg( 'time', time(),  get_stylesheet_directory_uri() . '/' . $path );
+	return add_query_arg('time', time(),  get_stylesheet_directory_uri() . '/' . $path);
 }
 
 /**
@@ -123,19 +129,20 @@ function tailpress_asset( $path ) {
  *
  * @return array
  */
-function tailpress_nav_menu_add_li_class( $classes, $item, $args, $depth ) {
-	if ( isset( $args->li_class ) ) {
+function symbos_nav_menu_add_li_class($classes, $item, $args, $depth)
+{
+	if (isset($args->li_class)) {
 		$classes[] = $args->li_class;
 	}
 
-	if ( isset( $args->{"li_class_$depth"} ) ) {
+	if (isset($args->{"li_class_$depth"})) {
 		$classes[] = $args->{"li_class_$depth"};
 	}
 
 	return $classes;
 }
 
-add_filter( 'nav_menu_css_class', 'tailpress_nav_menu_add_li_class', 10, 4 );
+add_filter('nav_menu_css_class', 'symbos_nav_menu_add_li_class', 10, 4);
 
 /**
  * Adds option 'submenu_class' to 'wp_nav_menu'.
@@ -146,12 +153,13 @@ add_filter( 'nav_menu_css_class', 'tailpress_nav_menu_add_li_class', 10, 4 );
  *
  * @return array
  */
-function tailpress_nav_menu_add_submenu_class( $classes, $args, $depth ) {
-	if ( isset( $args->submenu_class ) ) {
+function symbos_nav_menu_add_submenu_class($classes, $args, $depth)
+{
+	if (isset($args->submenu_class)) {
 		$classes[] = $args->submenu_class;
 	}
 
-	if ( isset( $args->{"submenu_class_$depth"} ) ) {
+	if (isset($args->{"submenu_class_$depth"})) {
 		$classes[] = $args->{"submenu_class_$depth"};
 	}
 
