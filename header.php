c19f3e57b3d06333a6d3a6d8b756238d97879a94
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-06-04 09:41:46 +0200

    Update fixed social icons
    - Add responsive breakpoints
    - Add currentColor fill to linkedin svg to be able to change both icons in one swoop

--- header.php
+++ header.php
@@ -12,16 +12,16 @@
 
 <body <?php body_class('bg-cod-gray-500 text-white antialiased font-blinker flex flex-col'); ?>>
 
-	<nav class="fixed z-50 flex flex-col gap-2 bottom-4 left-4 text-cod-gray-500 ">
-		<a class="flex items-center w-10 h-10 p-2 bg-white rounded-full" href="https://github.com/nikolailehbrink" target="_blank" rel="noopener noreferrer">
+	<nav class="fixed z-50 flex flex-col gap-2 lg:bottom-4 max-lg:right-2 bottom-2 lg:left-4 text-cod-gray-500 ">
 
+		<a class="flex items-center w-10 h-10 p-2 bg-white rounded-full" href="https://github.com/nikolailehbrink" target="_blank" rel="noopener noreferrer">
 			<svg viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg">
 				<path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="currentColor" />
 			</svg>
 		</a>
-		<a class="flex items-center justify-center w-10 h-10 p-3 bg-white rounded-full" href="https://linkedin.com/in/nikolailehbrink/" target="_blank" rel="noopener noreferrer">
+		<a class="flex items-center w-10 h-10 p-3 bg-white rounded-full" href="https://linkedin.com/in/nikolailehbrink/" target="_blank" rel="noopener noreferrer">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-				<path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+				<path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" fill="currentColor" />
 			</svg>
 		</a>
 	</nav>
