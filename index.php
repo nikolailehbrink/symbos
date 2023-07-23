adcfd5b313548c5cd42fb4b683c4aabf07a38457
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-07-23 23:11:48 +0200

    Remove demo content

--- index.php
+++ index.php
@@ -15,16 +15,5 @@
 	<?php endif; ?>
 
 </div>
-
-<!-- Slider main container -->
-<div class="swiper projects">
-	<div class="swiper-wrapper">
-		<div class="swiper-slide">Slide 1</div>
-		<div class="swiper-slide">Slide 2</div>
-		<div class="swiper-slide">Slide 3</div>
-		<div class="swiper-slide">Slide 4</div>
-	</div>
-	<div class="swiper-pagination"></div>
-</div>
 <?php
 get_footer();
